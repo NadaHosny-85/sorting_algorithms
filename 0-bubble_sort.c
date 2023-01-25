6cec37f3b51da98fccc73515191510d91fdfe3ff
Nada Hosny <nadahosni96@gmail.com>
2023-01-25 03:00:50 -0800

    added another fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,11 +23,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
 				print_array(array, size);
 			}
 		}

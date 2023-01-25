dc98aa2ebbe4ae98d6524ec874d79da03101d5eb
Nada Hosny <nadahosni96@gmail.com>
2023-01-25 05:55:30 -0800

    added bonus task 100

---
+++ 100-shell_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * shell_sort - function sorts array using
+ * shell algorithm 
+ *
+ * @array: parameter 1
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k = 0;
+	int temp = 0;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	while (k < size / 3)
+	{
+		k = k * 3 + 1;
+	}
+	for (; k > 0; k = (k - 1) / 3)
+	{
+		for (i = k; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= k && array[j - k] > temp; j -= k)
+			{
+				if (array[j] != array[j - k])
+				{
+					array[j] = array[j - k];
+				}
+				if (array[j] != temp)
+				{
+					array[j] = temp;
+				}
+			}
+		}
+		print_array(array, size);
+	}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * quick_sort - function sorts array using
+ * quick sort algorithm
+ *
+ * @array: parameter 1
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+
